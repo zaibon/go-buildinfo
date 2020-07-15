5c40a051e876af9bed1ae8871a6583ca67b23527
Naitik Shah <n@daaku.org>
2020-07-15 22:13:04 +0400

    a little more real releaseVersion

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import * as core from '@actions/core';
 
 async function run(): Promise<void> {
   try {
-    const releaseVersion: string = core.getInput('release_version');
+    const ldflags = [];
+
+    let releaseVersion: string = core.getInput('release_version');
+    if (releaseVersion === '') {
+      releaseVersion = process.env['GITHUB_RUN_NUMBER'] || 'unknown';
+    }
+    ldflags.push(`-X github.com/daaku/buildinfo.releaseVersion=${releaseVersion}`)
+
     core.debug(`Release Version: ${releaseVersion}`);
     for (const key of Object.keys(process.env)) {
       core.debug(`${key}=${process.env[key]}`);
     }
-    core.exportVariable('BI_LDFLAGS', 'foobar');
+    core.exportVariable('BI_LDFLAGS', ldflags.join(' '));
   } catch (error) {
     core.setFailed(error.message);
   }
