2824f7882912d742ecf0fc4533fdb59f2e18d7ca
Naitik Shah <n@daaku.org>
2020-07-15 22:28:13 +0400

    populate all the bits

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,44 @@
 import * as core from '@actions/core';
 
+function releaseVersion(): string {
+  let releaseVersion: string = core.getInput('release_version');
+  if (releaseVersion !== '') {
+    return releaseVersion;
+  }
+
+  const ref = process.env.GITHUB_REF;
+  if (ref) {
+    return ref;
+  }
+
+  const runNumber = process.env.GITHUB_RUN_NUMBER;
+  if (runNumber) {
+    return `v${runNumber}`;
+  }
+
+  return 'unknown';
+}
+
+function buildURL(): string {
+  return `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`
+}
+
+function buildHash(): string {
+  return process.env.GITHUB_SHA || 'unknown';
+}
+
+function buildTimeUnix(): string {
+  return `${Math.round(Date.now()/1000)}`;
+}
+
 async function run(): Promise<void> {
   try {
-    const ldflags = [];
-
-    let releaseVersion: string = core.getInput('release_version');
-    if (releaseVersion === '') {
-      releaseVersion = process.env['GITHUB_RUN_NUMBER'] || 'unknown';
-    }
-    ldflags.push(
-      `-X github.com/daaku/buildinfo.releaseVersion=${releaseVersion}`
-    );
-
-    core.debug(`Release Version: ${releaseVersion}`);
-    for (const key of Object.keys(process.env)) {
-      core.debug(`${key}=${process.env[key]}`);
-    }
+    const ldflags = [
+      `-X github.com/daaku/buildinfo.releaseVersion=${releaseVersion()}`,
+      `-X github.com/daaku/buildinfo.buildHash=${buildHash()}`,
+      `-X github.com/daaku/buildinfo.buildTimeUnix=${buildTimeUnix()}`,
+      `-X github.com/daaku/buildinfo.buildURL=${buildURL()}`,
+    ];
     core.exportVariable('BI_LDFLAGS', ldflags.join(' '));
   } catch (error) {
     core.setFailed(error.message);
